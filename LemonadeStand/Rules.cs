93552b3d7fe121b389ab3679ed2197f8885233f3
Robe Gogan <robe.gogan@gmail.comgit config --global user.email robe.gogan@gmail.comgit push origin master:qahthing>
2019-01-31 15:14:11 -0600

    static rules class being constructed with a case/switch to allow users to review information about the game rules.

--- LemonadeStand/Rules.cs
+++ LemonadeStand/Rules.cs
@@ -10,7 +10,20 @@ namespace LemonadeStand
 
         static void ViewRules()
         {
-           
+            Console.WriteLine("What information are you looking for? game objective(1) how to make money(2) recipes(3) weather(4)");
+            string ruleChoice = Console.ReadLine();
+            switch (ruleChoice)
+            {
+                case "1":
+                    Console.WriteLine("You own a lemonade stand and you goal is to turn a profit in the first week. You may continue as many weeks as you would like.");
+                    break;
+                case "2":
+                    Console.WriteLine("Making money is simply how much lemonade you can sell. The better the taste and what price you choose to determine for supply and demand will determine how much money you will have.");
+                    break;
+                default:
+                    Console.WriteLine("Please choose a value from 1 to 4");
+                    break;
+            }
         }
     }
 }
